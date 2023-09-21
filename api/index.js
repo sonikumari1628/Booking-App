8330eccea2704ccbc0fb3ea432315ebeda4a8d4c
sonikumari1628 <sonitiwari1625@gmail.com>
2023-09-21 21:48:31 +0530

    initial commit

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+
+import path from "path"
 import authRoute from "./routes/auth.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
@@ -41,6 +43,23 @@ app.use("/api/auth", authRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
 app.use("/api/users", usersRoute);
+
+//deployment
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  console.log("in production");
+  app.use(express.static(path.join(__dirname1, "/booking/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "booking", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is Running..");
+  });
+}
+
 
 
 //checkout
--- booking/src/pages/home/Home.jsx
+++ booking/src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import Featured from "../../components/featured/Featured";
-import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
+// import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
 import MailList from "../../components/mailList/MailList";
@@ -16,8 +16,8 @@ const Home = () => {
         <Featured/>
         <h1 className="homeTitle">Browse by property type</h1>
         <PropertyList/>
-        <h1 className="homeTitle">Homes guests love</h1>
-        <FeaturedProperties/>
+        {/* <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties/> */}
         <MailList/>
         <Footer/>
       </div>
